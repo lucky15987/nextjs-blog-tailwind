537f94fc14a182f0209e097a41d6ce8f95258917
Ethan N <lucky35789@yahoo.com>
2022-08-08 11:58:23 -0400

    added a "Head" tag to first post using import from "next/head"

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1 className='font-bold text-5xl'>First Post</h1>
             <h2>
                 <Link href="/">
