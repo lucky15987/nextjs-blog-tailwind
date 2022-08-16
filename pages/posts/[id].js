e1c1b872009059ee4b39b1592956a13765fa5a98
Ethan N <lucky35789@yahoo.com>
2022-08-16 13:32:28 -0400

    added mark down imports (remark and remark-html)

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,12 +4,14 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-    </Layout>
+    {postData.title}
+    <br />
+    {postData.id}
+    <br />
+    {postData.date}
+    <br />
+    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+  </Layout>
   )
 }
 
@@ -22,7 +24,8 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  //add the "await" keyword:
+    const postData = await getPostData(params.id)
   return {
     props: {
       postData
